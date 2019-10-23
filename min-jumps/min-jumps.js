afa4b10e768682dfc782e9264dd69feec7a681a8
Megan Donnelly <md2942@nyu.edu>
2019-10-23 19:15:40 -0400

    commented out solution code different than current solution

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,19 +2,33 @@
 
 // Complete this algo
 function minJumps(arr) {
-    let jumpCount = 0
-    let spaces = 0
-    let max = 0
-    for(let i = 0; i < arr.length; i++){
-        if(spaces < i){
-            jumpCount++
-            spaces = max
-        } else if (spaces > i) {
-             return jumpCount  
-        }
-        max = Math.max(max, arr[i] + i)      
+  let jumpCount = 0;
+  let spaces = 0;
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (spaces < i) {
+      jumpCount++;
+      spaces = max;
+    } else if (spaces > i) {
+      return jumpCount;
     }
-    return jumpCount;   
+    max = Math.max(max, arr[i] + i);
+  }
+  return jumpCount;
 }
-  
-module.exports = minJumps         
+
+// const minJumps = arr => {
+// 	let jumps = arr.map(el => Infinity)
+// 	jumps[0] = 0;
+
+// 	for(let i = 1; i < jumps.length; i++){
+// 		for(let j = 0; j <= i; j++){
+// 			if(j + arr[j] >= i) {
+// 				jumps[i] = Math.min(jumps[i], jumps[j] + 1)
+// 			}
+// 		}
+// 	}
+// 	return jumps[arr.length - 1];
+// };
+
+module.exports = minJumps;
