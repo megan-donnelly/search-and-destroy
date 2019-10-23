dfac484757aa009c41a7975afe4fa33e1e3bd70c
EthanG19 <eth13gou2514@gmail.com>
2019-10-23 17:55:23 -0400

    m

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,20 @@
 'use strict';
 
 // Complete this algo
-const minJumps = arr => {
-
-};
-
-module.exports = minJumps
+function minJumps(arr) {
+    let jumpCount = 0
+    let spaces = 0
+    let max = 0
+    for(let i = 0; i < arr.length; i++){
+        if(spaces < i){
+            jumpCount++
+            spaces = max
+        } else if (spaces > i) {
+             return jumpCount  
+        }
+        max = Math.max(max, arr[i] + i)      
+    }
+    return jumpCount;   
+}
+  
+module.exports = minJumps         
