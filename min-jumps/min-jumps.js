f87162774896203c5ff19e7ab86f40db0c46f391
Megan Donnelly <md2942@nyu.edu>
2019-10-24 17:32:37 -0400

    third option for jump solution

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,21 +1,37 @@
 'use strict';
 
 // Complete this algo
-function minJumps(arr) {
-  let jumpCount = 0;
-  let spaces = 0;
-  let max = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (spaces < i) {
-      jumpCount++;
-      spaces = max;
-    } else if (spaces > i) {
-      return jumpCount;
+const minJumps = arr => {
+  let jumps = 0;
+  let steps = arr[0];
+  let maxReach = arr[0];
+  for (let i = 1; i < arr.length - 1; i++) {
+    maxReach = Math.max(maxReach, arr[i] + i);
+    steps--;
+    if (steps === 0) {
+      jumps++;
+      steps = maxReach - i;
     }
-    max = Math.max(max, arr[i] + i);
   }
-  return jumpCount;
-}
+  console.log(jumps + 1);
+  return jumps + 1;
+};
+
+// function minJumps(arr) {
+//   let jumpCount = 0;
+//   let spaces = 0;
+//   let max = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (spaces < i) {
+//       jumpCount++;
+//       spaces = max;
+//     } else if (spaces > i) {
+//       return jumpCount;
+//     }
+//     max = Math.max(max, arr[i] + i);
+//   }
+//   return jumpCount;
+// }
 
 // const minJumps = arr => {
 // 	let jumps = arr.map(el => Infinity)
